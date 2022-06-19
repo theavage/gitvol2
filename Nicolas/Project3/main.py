2ebd61f4e73d013d3aab87086ac2c12719596330
s212781 <104715843+s212781@users.noreply.github.com>
2022-06-19 11:43:20 +0200

    Add files via upload

--- Nicolas/Project3/main.py
+++ Nicolas/Project3/main.py
@@ -11,6 +11,7 @@ from predict import *
 
 PREDICT = False
 
+test_path = '/dtu/datasets1/02514/isic/train_allstyles/Segmentations'
 data_path_train_X = '/dtu/datasets1/02514/isic/train_allstyles/Images/*.jpg'
 data_path_train_y = '/dtu/datasets1/02514/isic/train_allstyles/Segmentations/*.png'
 
@@ -23,12 +24,22 @@ device = checkDevice()
 model = UNet().to(device)
 
 if PREDICT == False:
-    X_train_loader, X_val_loader, y_train_loader, y_val_loader = loadTrainData(data_path_train_X, data_path_train_y, 32)
+    X_train_loader, X_val_loader, y_train_loader, y_val_loader = loadTrainData(test_path, test_path, 32)
 
-    train(model, optim.Adam(model.parameters()), 20, X_train_loader, X_val_loader, y_train_loader, y_val_loader, device)
+    model = train(model, optim.Adam(model.parameters()), 20, X_train_loader, X_val_loader, y_train_loader, y_val_loader, device)
+
+    path = 'model.pt'
+    torch.save(model.state_dict(), path)    
 
 else:
+
+    path = 'model.pt'
+    model.load_state_dict(torch.load(path))
+
+    device = checkDevice()
+
     X_test_loader, y_test_loader = loadTestData(data_path_test_X, data_path_test_y, 32)
     pred = predict(model, X_test_loader, device) 
+    res = evaluate(pred, y_test_loader)
 
 
